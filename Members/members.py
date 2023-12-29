36b0b70eed8baffbff0f6a29b8aa1d4ee079937f
Adedayo UWensuyi <onehpee@yahoo.com>
2023-12-28 23:52:42 -0600

    setting up members of the bank

---
+++ Members/members.py
@@ -0,0 +1,13 @@
+class Members:
+    #constructor
+    def __init__(self, id, prefix, firstName, lastName, SOS, money = 0):
+        #instance attributes
+        self.id = id
+        self.prefix = prefix
+        self.firstName = firstName
+        self.lastName = lastName
+        self.SOS = SOS
+        self.money = money
+        
+    def deposit(self):
+        print("")
