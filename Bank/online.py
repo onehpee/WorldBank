e7e85d602670dc9c95ae9844996a7236af1d69a4
Adedayo UWensuyi <onehpee@yahoo.com>
2024-01-07 22:19:11 -0600

    Working on Password

--- Bank/online.py
+++ Bank/online.py
@@ -1,4 +1,4 @@
-
+import re
 class user:
     print("Welcome to World Bank Online")
     def __init__(self, username, password, balance):
@@ -9,3 +9,28 @@ class user:
     def signIn(self):
         self.username = str(input("Please Enter Your Username: "))
         self.password = str(input("Please Enter Your Password: "))
+        
+        
+    def passLenCheck(self):
+        if len(self.password) >= 8:
+            self.password = self.password
+        else:
+            print("This Password doesn't have 8 or more characters")
+    
+    def passSpecChar(self):
+        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+        if regex.search(self.password) == None:
+            self.password = self.password
+        else:
+            print("This Password doesn't contain a special character like: [@_!#$%^&*()<>?/\|}{~:]")
+            
+    def passUpperChar(self):
+        check = False
+        for char in self.password:
+            if char.isupper():
+                check = True
+                break
+        
+        
+if __name__ == '__main__' :
+    u = user
