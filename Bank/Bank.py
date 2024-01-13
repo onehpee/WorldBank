c1fc5def76c7f781aa3c57273d73fec055c99a4b
Adedayo UWensuyi <onehpee@yahoo.com>
2024-01-12 23:28:48 -0600

    currency convertor

--- Bank/Bank.py
+++ Bank/Bank.py
@@ -11,7 +11,7 @@ class Bank(members):
         
     
 class Currency_convertor:
-    # empty dict to store the conversion ratess
+    # empty dict to store the conversion ratesss
     rates = {} 
     def __init__(self, url):
         data = requests.get(url).json()
