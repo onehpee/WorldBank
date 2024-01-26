0c66721e5c3bd9483450b8021f6ea6c50fca2511
Adedayo UWensuyi <onehpee@yahoo.com>
2024-01-25 23:00:50 -0600

    working on deposits and withdraws and menu

--- Bank/online.py
+++ Bank/online.py
@@ -1,16 +1,36 @@
 import re
-class user:
+class app:
     print("Welcome to World Bank Online")
     def __init__(self, username, password):
         self.username = str(input("Please Enter Your Username: "))
         self.password = str(input("Please Enter Your Password: "))
         
-    def create_account(self):
-      credit_card_number = "400000" + format(randint(0000000000, 9999999999), '010d')
-      pin_number = format(randint(0000, 9999), '04d')
+    # def home():
+    #     print("\n>> World Bank Home:")
+    #     print("1. Create New Profile\n2. Login\n3. Info\n4. Log Out")
+    #     Task=int(input("> Please select your option (1/2/3/4): "))
+    #     while(Task !='4'):
+    #         if (Task==1):
+    #             #new proflie
+    #         elif(Task==2):
+    #             #login
+    #         elif(Task==3):
+    #             #info
+    #         elif(Task==4):
+    #             print("=== GOOD BYE, SEE YOU AGAIN! ===\n\n")
+    #             main_menu()
+    #         else:
+    #             print("!!! Invalid Option! Please read the option carefully! !!!\n")
+    #     Task=int(input("Please select your option (1/2/3/4): "))
 
-      self.card_numbers.append(credit_card_number)
-      self.pin_numbers.append(pin_number)
+        
+    def create_account(self):
+        
+        credit_card_number = "400000" + format(randint(0000000000, 9999999999), '010d')
+        pin_number = format(randint(0000, 9999), '04d')
+
+        self.card_numbers.append(credit_card_number)
+        self.pin_numbers.append(pin_number)
     
     def accountID(self):
         self.username = not yet
@@ -36,6 +56,9 @@ class user:
             if char.isupper():
                 check = True
                 break
+            
+            
+    
         
         
 if __name__ == '__main__' :
